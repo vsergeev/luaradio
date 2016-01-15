43130835aff92a2955e23d4e615fbc99e9deb656
Vanya Sergeev <vsergeev@gmail.com>
2016-01-15 01:11:02 -0800

    blocks/signal/summer: fix sample lookup in inputs

--- blocks/signal/summer.lua
+++ blocks/signal/summer.lua
@@ -5,16 +5,17 @@ local block = require('block')
 local SummerBlock = block.BlockFactory("SummerBlock")
 
 function SummerBlock:instantiate()
-    self.inputs = {pipe.PipeInput("x", types.AnyType),
-                   pipe.PipeInput("y", types.AnyType)}
-    self.outputs = {pipe.PipeOutput("out", types.AnyType,
+    self.inputs = {pipe.PipeInput("x", AnyType),
+                   pipe.PipeInput("y", AnyType)}
+    self.outputs = {pipe.PipeOutput("out", AnyType,
                     function () return self.inputs[1].pipe.rate end)}
 end
 
 function SummerBlock:process(x, y)
-    local out = ComplexIntegerType.alloc(x.length)
+    -- FIXME infer output type
+    local out = ComplexFloatType.alloc(x.length)
     for i = 0, x.length-1 do
-        out[i] = x[i] + y[i]
+        out.data[i] = x.data[i] + y.data[i]
     end
     return out
 end

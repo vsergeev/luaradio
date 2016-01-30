02560a9871e9c72c3def6e040be631c408887d0c
Vanya Sergeev <vsergeev@gmail.com>
2016-01-30 00:25:07 -0800

    blocks/signal/multiplier: add MultiplierBlock

---
+++ radio/blocks/signal/multiplier.lua
@@ -0,0 +1,32 @@
+local ffi = require('ffi')
+
+local block = require('radio.core.block')
+local ComplexFloat32Type = require('radio.types.complexfloat32').ComplexFloat32Type
+local Float32Type = require('radio.types.float32').Float32Type
+
+local MultiplierBlock = block.BlockFactory("MultiplierBlock")
+
+function MultiplierBlock:instantiate()
+    self:add_type_signature({block.Input("in1", ComplexFloat32Type), block.Input("in2", ComplexFloat32Type)}, {block.Output("out", ComplexFloat32Type)}, MultiplierBlock.process_complex)
+    self:add_type_signature({block.Input("in1", Float32Type), block.Input("in2", Float32Type)}, {block.Output("out", Float32Type)}, MultiplierBlock.process_float)
+end
+
+ffi.cdef[[
+void (*volk_32fc_x2_multiply_32fc_a)(complex_float32_t* cVector, const complex_float32_t* aVector, const complex_float32_t* bVector, unsigned int num_points);
+void (*volk_32f_x2_multiply_32f_a)(float32_t* cVector, const float32_t* aVector, const float32_t* bVector, unsigned int num_points);
+]]
+local libvolk = ffi.load("libvolk.so")
+
+function MultiplierBlock:process_complex(x, y)
+    local out = ComplexFloat32Type.vector(x.length)
+    libvolk.volk_32fc_x2_multiply_32fc_a(out.data, x.data, y.data, x.length)
+    return out
+end
+
+function MultiplierBlock:process_float(x, y)
+    local out = Float32Type.vector(x.length)
+    libvolk.volk_32f_x2_multiply_32f_a(out.data, x.data, y.data, x.length)
+    return out
+end
+
+return {MultiplierBlock = MultiplierBlock}
--- radio/init.lua
+++ radio/init.lua
@@ -8,6 +8,7 @@ return {
     FIRFilterBlock = require('radio.blocks.signal.firfilter').FIRFilterBlock,
     IIRFilterBlock = require('radio.blocks.signal.iirfilter').IIRFilterBlock,
     SummerBlock = require('radio.blocks.signal.summer').SummerBlock,
+    MultiplierBlock = require('radio.blocks.signal.multiplier').MultiplierBlock,
     DownsamplerBlock = require('radio.blocks.signal.downsampler').DownsamplerBlock,
     FrequencyDiscriminatorBlock = require('radio.blocks.signal.frequencydiscriminator').FrequencyDiscriminatorBlock,
     FileDescriptorSinkBlock = require('radio.blocks.sinks.filedescriptorsink').FileDescriptorSinkBlock,

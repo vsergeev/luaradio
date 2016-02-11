0b2d29fa2a6137a0cc0695e3f82c929e7719e992
Vanya Sergeev <vsergeev@gmail.com>
2016-02-11 00:17:53 -0800

    core/vector: add capacity field, resize() and append() methods

--- radio/core/vector.lua
+++ radio/core/vector.lua
@@ -21,6 +21,40 @@ local PAGE_SIZE = ffi.C.sysconf(ffi.C._SC_PAGESIZE)
 
 local Vector = object.class_factory()
 
+function Vector:resize(num)
+    -- If we're within capacity, adjust length and size
+    if num <= self.capacity then
+        self.length = num
+        self.size = num*ffi.sizeof(self.type)
+        return
+    end
+
+    -- Calculate new capacity
+    local capacity = math.max(1, 2*self.capacity)
+    -- Calculate new buffer size
+    local size = capacity*ffi.sizeof(self.type)
+    -- Allocate buffer
+    local buf = ffi.gc(ffi.C.aligned_alloc(PAGE_SIZE, size), ffi.C.free)
+    -- Zero buffer
+    ffi.C.memset(buf, 0, size)
+    -- Cast to specified pointer type
+    local ptr = ffi.cast(ffi.typeof("$ *", self.type), buf)
+    -- Copy old data
+    ffi.C.memcpy(buf, self._buffer, math.min(self.size, num*ffi.sizeof(self.type)))
+
+    -- Adjust state
+    self.data = ptr
+    self.length = num
+    self.capacity = capacity
+    self.size = num*ffi.sizeof(self.type)
+    self._buffer = buf
+end
+
+function Vector:append(elem)
+    self:resize(self.length + 1)
+    self.data[self.length - 1] = elem
+end
+
 -- Constructors
 
 function Vector.new(ctype, num)
@@ -36,7 +70,7 @@ function Vector.new(ctype, num)
     local ptr = ffi.cast(ffi.typeof("$ *", ctype), buf)
 
     -- Return vector container
-    return setmetatable({data = ptr, length = num, size = size, type = ctype, _buffer = buf}, Vector)
+    return setmetatable({data = ptr, length = num, capacity = num, size = size, type = ctype, _buffer = buf}, Vector)
 end
 
 function Vector.cast(ctype, buf, size)
@@ -46,7 +80,7 @@ function Vector.cast(ctype, buf, size)
     local ptr = ffi.cast(ffi.typeof("const $ *", ctype), buf)
 
     -- Return vector container
-    return setmetatable({data = ptr, length = num, size = size, type = ctype, _buffer = buf}, Vector)
+    return setmetatable({data = ptr, length = num, capacity = num, size = size, type = ctype, _buffer = buf}, Vector)
 end
 
 return {Vector = Vector, PAGE_SIZE = PAGE_SIZE}

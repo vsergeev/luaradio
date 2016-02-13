a0d1d752372912c1303460229ea755c832e1d299
Vanya Sergeev <vsergeev@gmail.com>
2016-02-12 16:37:27 -0800

    types/byte: use CStructType factory to build type

--- radio/types/byte.lua
+++ radio/types/byte.lua
@@ -1,7 +1,6 @@
 local ffi = require('ffi')
 
-local object = require('radio.core.object')
-local Vector = require('radio.core.vector').Vector
+local CStructType = require('radio.types.cstruct').CStructType
 
 ffi.cdef[[
 typedef struct {
@@ -9,10 +8,7 @@ typedef struct {
 } byte_t;
 ]]
 
-local ByteType
-local mt = object.class_factory()
-
--- Operations
+local mt = {}
 
 function mt:__add(other)
     return self.new(self.value + other.value)
@@ -46,41 +42,6 @@ function mt:__tostring()
     return "Byte<value=" .. self.value .. ">"
 end
 
--- Constructors
-
-function mt.new(value)
-    return ByteType(value)
-end
-
-function mt.vector(num)
-    return Vector(ByteType, num)
-end
-
-function mt.vector_from_array(arr)
-    local vec = Vector(ByteType, #arr)
-    for i = 0, vec.length-1 do
-        vec.data[i] = ByteType(arr[i+1])
-    end
-    return vec
-end
-
--- Buffer serialization interface
-
-function mt.serialize(vec)
-    return vec.data, vec.size
-end
-
-function mt.deserialize(buf, count)
-    local size = count*ffi.sizeof(ByteType)
-    return Vector.cast(ByteType, buf, size), size
-end
-
-function mt.deserialize_count(buf, size)
-    return math.floor(size/ffi.sizeof(ByteType))
-end
-
--- FFI type binding
-
-ByteType = ffi.metatype("byte_t", mt)
+local ByteType = CStructType.factory("byte_t", mt)
 
 return {ByteType = ByteType}

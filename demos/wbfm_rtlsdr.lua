faced306c60cb269bfc10bd5d5196a74082852a0
Vanya Sergeev <vsergeev@gmail.com>
2016-02-12 19:33:54 -0800

    demos/wbfm_rtlsdr: make cosmetic simplifications

--- demos/wbfm_rtlsdr.lua
+++ demos/wbfm_rtlsdr.lua
@@ -8,13 +8,13 @@ end
 local frequency = tonumber(arg[1])
 local offset = -600e3
 
+local top = radio.CompositeBlock()
 local b0 = radio.RtlSdrSourceBlock(frequency + offset, 2048000)
 local b1 = radio.TunerBlock(offset, 190e3, 10)
 local b2 = radio.FrequencyDiscriminatorBlock(6.0)
 local b3 = radio.FMDeemphasisFilterBlock(75e-6)
 local b4 = radio.DecimatorBlock(15e3, 4)
 local b5 = radio.PulseAudioSinkBlock()
-local top = radio.CompositeBlock()
 
 top:connect(b0, b1, b2, b3, b4, b5)
-top:run(true)
+top:run()

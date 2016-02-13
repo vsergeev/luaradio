039d0e5f6ddbff01076be00bccf7f3b17c868870
Vanya Sergeev <vsergeev@gmail.com>
2016-02-12 16:36:41 -0800

    types/float32: use CStructType factory to build type

--- radio/types/float32.lua
+++ radio/types/float32.lua
@@ -1,7 +1,6 @@
 local ffi = require('ffi')
 
-local object = require('radio.core.object')
-local Vector = require('radio.core.vector').Vector
+local CStructType = require('radio.types.cstruct').CStructType
 
 ffi.cdef[[
 typedef struct {
@@ -9,10 +8,7 @@ typedef struct {
 } float32_t;
 ]]
 
-local Float32Type
-local mt = object.class_factory()
-
--- Operations
+local mt = {}
 
 function mt:__add(other)
     return self.new(self.value + other.value)
@@ -46,41 +42,6 @@ function mt:__tostring()
     return "Float32<value=" .. self.value .. ">"
 end
 
--- Constructors
-
-function mt.new(value)
-    return Float32Type(value)
-end
-
-function mt.vector(num)
-    return Vector(Float32Type, num)
-end
-
-function mt.vector_from_array(arr)
-    local vec = Vector(Float32Type, #arr)
-    for i = 0, vec.length-1 do
-        vec.data[i] = Float32Type(arr[i+1])
-    end
-    return vec
-end
-
--- Buffer serialization interface
-
-function mt.serialize(vec)
-    return vec.data, vec.size
-end
-
-function mt.deserialize(buf, count)
-    local size = count*ffi.sizeof(Float32Type)
-    return Vector.cast(Float32Type, buf, size), size
-end
-
-function mt.deserialize_count(buf, size)
-    return math.floor(size/ffi.sizeof(Float32Type))
-end
-
--- FFI type binding
-
-Float32Type = ffi.metatype("float32_t", mt)
+local Float32Type = CStructType.factory("float32_t", mt)
 
 return {Float32Type = Float32Type}

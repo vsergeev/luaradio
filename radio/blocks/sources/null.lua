8b899345633e8be2c480038ae4a7f5906a9d9b3d
Vanya Sergeev <vsergeev@gmail.com>
2016-02-21 16:20:21 -0800

    blocks/sources/null: remove underscore for private variables

--- radio/blocks/sources/null.lua
+++ radio/blocks/sources/null.lua
@@ -4,15 +4,15 @@ local ComplexFloat32Type = require('radio.types.complexfloat32').ComplexFloat32T
 local NullSource = block.factory("NullSource")
 
 function NullSource:instantiate(rate)
-    self._rate = rate or 1
-    self._chunk_size = 8192
-    self.out = ComplexFloat32Type.vector(self._chunk_size)
+    self.rate = rate or 1
+    self.chunk_size = 8192
+    self.out = ComplexFloat32Type.vector(self.chunk_size)
 
     self:add_type_signature({}, {block.Output("out", ComplexFloat32Type)})
 end
 
 function NullSource:get_rate()
-    return self._rate
+    return self.rate
 end
 
 function NullSource:process()

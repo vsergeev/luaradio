30b256f14af9d6a75d0168e9fa0795bea7887317
Vanya Sergeev <vsergeev@gmail.com>
2016-02-26 01:39:32 -0800

    blocks/signal/multiply: add support for pure lua implementation

--- radio/blocks/signal/multiply.lua
+++ radio/blocks/signal/multiply.lua
@@ -1,5 +1,6 @@
 local ffi = require('ffi')
 
+local platform = require('radio.core.platform')
 local block = require('radio.core.block')
 local ComplexFloat32Type = require('radio.types.complexfloat32').ComplexFloat32Type
 local Float32Type = require('radio.types.float32').Float32Type
@@ -11,22 +12,48 @@ function MultiplyBlock:instantiate()
     self:add_type_signature({block.Input("in1", Float32Type), block.Input("in2", Float32Type)}, {block.Output("out", Float32Type)}, MultiplyBlock.process_real)
 end
 
-ffi.cdef[[
-void (*volk_32fc_x2_multiply_32fc_a)(complex_float32_t* cVector, const complex_float32_t* aVector, const complex_float32_t* bVector, unsigned int num_points);
-void (*volk_32f_x2_multiply_32f_a)(float32_t* cVector, const float32_t* aVector, const float32_t* bVector, unsigned int num_points);
-]]
-local libvolk = ffi.load("libvolk.so")
+if platform.features.volk then
 
-function MultiplyBlock:process_complex(x, y)
-    local out = ComplexFloat32Type.vector(x.length)
-    libvolk.volk_32fc_x2_multiply_32fc_a(out.data, x.data, y.data, x.length)
-    return out
-end
+    ffi.cdef[[
+    void (*volk_32fc_x2_multiply_32fc_a)(complex_float32_t* cVector, const complex_float32_t* aVector, const complex_float32_t* bVector, unsigned int num_points);
+    void (*volk_32f_x2_multiply_32f_a)(float32_t* cVector, const float32_t* aVector, const float32_t* bVector, unsigned int num_points);
+    ]]
+    local libvolk = platform.libs.volk
 
-function MultiplyBlock:process_real(x, y)
-    local out = Float32Type.vector(x.length)
-    libvolk.volk_32f_x2_multiply_32f_a(out.data, x.data, y.data, x.length)
-    return out
+    function MultiplyBlock:process_complex(x, y)
+        local out = ComplexFloat32Type.vector(x.length)
+        libvolk.volk_32fc_x2_multiply_32fc_a(out.data, x.data, y.data, x.length)
+        return out
+    end
+
+    function MultiplyBlock:process_real(x, y)
+        local out = Float32Type.vector(x.length)
+        libvolk.volk_32f_x2_multiply_32f_a(out.data, x.data, y.data, x.length)
+        return out
+    end
+
+else
+
+    function MultiplyBlock:process_complex(x, y)
+        local out = ComplexFloat32Type.vector(x.length)
+
+        for i = 0, x.length - 1 do
+            out.data[i] = x.data[i] * y.data[i]
+        end
+
+        return out
+    end
+
+    function MultiplyBlock:process_real(x, y)
+        local out = Float32Type.vector(x.length)
+
+        for i = 0, x.length - 1 do
+            out.data[i] = x.data[i] * y.data[i]
+        end
+
+        return out
+    end
+
 end
 
 return {MultiplyBlock = MultiplyBlock}

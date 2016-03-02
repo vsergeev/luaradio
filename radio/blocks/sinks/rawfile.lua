59e8bc7575205e6afbef7609aa9b8e123c3b0c15
Vanya Sergeev <vsergeev@gmail.com>
2016-03-01 22:43:44 -0800

    blocks/sinks/rawfile: use file stream interface for file i/o

--- radio/blocks/sinks/rawfile.lua
+++ radio/blocks/sinks/rawfile.lua
@@ -15,24 +15,30 @@ function RawFileSink:instantiate(file)
     self:add_type_signature({block.Input("in", function (type) return true end)}, {})
 end
 
+-- File I/O
 ffi.cdef[[
     typedef struct FILE FILE;
     FILE *fopen(const char *path, const char *mode);
-    int fileno(FILE *stream);
-    int write(int fd, const void *buf, size_t count);
+    FILE *fdopen(int fd, const char *mode);
+    size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
 ]]
 
 function RawFileSink:initialize()
     if self.filename then
-        self.file = ffi.C.fopen(self.filename, "w")
+        self.file = ffi.C.fopen(self.filename, "wb")
         assert(self.file ~= nil, "fopen(): " .. ffi.string(ffi.C.strerror(ffi.errno())))
-        self.fd = ffi.C.fileno(self.file)
+    else
+        self.file = ffi.C.fdopen(self.fd, "wb")
+        assert(self.file ~= nil, "fdopen(): " .. ffi.string(ffi.C.strerror(ffi.errno())))
     end
 end
 
 function RawFileSink:process(x)
     local data, size = x.type.serialize(x)
-    assert(ffi.C.write(self.fd, data, size) == size, "write(): " .. ffi.string(ffi.C.strerror(ffi.errno())))
+
+    -- Write to file
+    local bytes_written = ffi.C.fwrite(data, 1, size, self.file)
+    assert(bytes_written == size, "fwrite(): " .. ffi.string(ffi.C.strerror(ffi.errno())))
 end
 
 return {RawFileSink = RawFileSink}

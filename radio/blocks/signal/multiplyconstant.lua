d032ccfda67b3065b901604b82aff7c9395d9c10
Vanya Sergeev <vsergeev@gmail.com>
2016-03-05 03:00:03 -0800

    blocks/signal/multiplyconstant: add MultiplyConstantBlock

---
+++ radio/blocks/signal/multiplyconstant.lua
@@ -0,0 +1,56 @@
+local block = require('radio.core.block')
+local object = require('radio.core.object')
+local types = require('radio.types')
+
+local MultiplyConstantBlock = block.factory("MultiplyConstantBlock")
+
+function MultiplyConstantBlock:instantiate(constant)
+    -- Convert constant to Float32Type or ComplexFloat32Type
+    if object.isinstanceof(constant, "number") then
+        self.constant = types.Float32Type(constant)
+    elseif object.isinstanceof(constant, types.Float32Type) then
+        self.constant = constant
+    elseif object.isinstanceof(constant, types.ComplexFloat32Type) then
+        self.constant = constant
+    else
+        error("Unsupported constant type.")
+    end
+
+    if object.isinstanceof(constant, types.ComplexFloat32Type) then
+        -- Only allow complex inputs for a complex constant
+        self:add_type_signature({block.Input("in", types.ComplexFloat32Type)}, {block.Output("out", types.ComplexFloat32Type)}, self.process_complex_by_complex)
+    else
+        self:add_type_signature({block.Input("in", types.Float32Type)}, {block.Output("out", types.Float32Type)}, self.process_float_by_float)
+        self:add_type_signature({block.Input("in", types.ComplexFloat32Type)}, {block.Output("out", types.ComplexFloat32Type)}, self.process_complex_by_float)
+    end
+end
+
+function MultiplyConstantBlock:initialize()
+    self.data_type = self.signature.inputs[1].data_type
+end
+
+function MultiplyConstantBlock:process_complex_by_complex(x)
+    local out = self.data_type.vector(x.length)
+    for i = 0, x.length - 1 do
+        out.data[i] = x.data[i] * self.constant
+    end
+    return out
+end
+
+function MultiplyConstantBlock:process_float_by_float(x)
+    local out = self.data_type.vector(x.length)
+    for i = 0, x.length - 1 do
+        out.data[i] = x.data[i] * self.constant
+    end
+    return out
+end
+
+function MultiplyConstantBlock:process_complex_by_float(x)
+    local out = self.data_type.vector(x.length)
+    for i = 0, x.length - 1 do
+        out.data[i] = x.data[i]:scalar_mul(self.constant.value)
+    end
+    return out
+end
+
+return {MultiplyConstantBlock = MultiplyConstantBlock}
--- tests/generate/generate_block_specs.py
+++ tests/generate/generate_block_specs.py
@@ -262,6 +262,28 @@ def generate_multiplyconjugate_spec():
 
     return vectors
 
+@block_spec("MultiplyConstantBlock", "tests/blocks/signal/multiplyconstant_spec.lua")
+def generate_multiplyconstant_spec():
+    def process(constant, x):
+        return [x * constant]
+
+    vectors = []
+    x = random_complex64(256)
+    y = random_float32(256)
+
+    # ComplexFloat32 vector times number constant
+    vectors.append(generate_test_vector(process, [2.5], [x], "Number constant, 256 ComplexFloat32 inputs, 256 ComplexFloat32 output"))
+    # ComplexFloat32 vector times float32 constant
+    vectors.append(generate_test_vector(process, [numpy.float32(3.5)], [x], "Float32 constant, 256 ComplexFloat32 inputs, 256 ComplexFloat32 output"))
+    # ComplexFloat32 vector times ComplexFloat32 constant
+    vectors.append(generate_test_vector(process, [numpy.complex64(complex(1,2))], [x], "ComplexFloat32 constant, 256 ComplexFloat32 inputs, 256 ComplexFloat32 output"))
+    # Float32 vector times number constant
+    vectors.append(generate_test_vector(process, [2.5], [y], "Number constant, 256 Float32 inputs, 256 Float32 output"))
+    # Float32 vector times Float32 constant
+    vectors.append(generate_test_vector(process, [numpy.float32(3.5)], [y], "Float32 constant, 256 Float32 inputs, 256 Float32 output"))
+
+    return vectors
+
 @block_spec("SumBlock", "tests/blocks/signal/sum_spec.lua")
 def generate_sum_spec():
     def process(x, y):

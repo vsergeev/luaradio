e302634920c88a07bb044ccbbec5fb4389133fdf
Vanya Sergeev <vsergeev@gmail.com>
2016-03-15 23:58:52 -0700

    composites/rationalresampler: compensate for upsampler attenuation

--- radio/composites/rationalresampler.lua
+++ radio/composites/rationalresampler.lua
@@ -2,6 +2,7 @@ local block = require('radio.core.block')
 local types = require('radio.types')
 
 local CompositeBlock = require('radio.core.composite').CompositeBlock
+local MultiplyConstantBlock = require('radio.blocks.signal.multiplyconstant').MultiplyConstantBlock
 local UpsamplerBlock = require('radio.blocks.signal.upsampler').UpsamplerBlock
 local LowpassFilterBlock = require('radio.blocks.signal.lowpassfilter').LowpassFilterBlock
 local DownsamplerBlock = require('radio.blocks.signal.downsampler').DownsamplerBlock
@@ -15,12 +16,13 @@ function RationalResamplerBlock:instantiate(bandwidth, interpolation, decimation
     self:add_type_signature({block.Input("in", types.ComplexFloat32Type)}, {block.Output("out", types.ComplexFloat32Type)})
     self:add_type_signature({block.Input("in", types.Float32Type)}, {block.Output("out", types.Float32Type)})
 
+    local scaler = MultiplyConstantBlock(interpolation)
     local upsampler = UpsamplerBlock(interpolation)
     local filter = LowpassFilterBlock(options.num_taps or 128, bandwidth)
     local downsampler = DownsamplerBlock(decimation)
 
-    self:connect(self, "in", upsampler, "in")
-    self:connect(upsampler, filter, downsampler)
+    self:connect(self, "in", scaler, "in")
+    self:connect(scaler, upsampler, filter, downsampler)
     self:connect(downsampler, "out", self, "out")
 end
 

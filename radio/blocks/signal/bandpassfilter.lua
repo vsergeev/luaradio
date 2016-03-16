dc96754ac1b14fe99e2be92c50c4732dd8daa45a
Vanya Sergeev <vsergeev@gmail.com>
2016-03-15 22:35:52 -0700

    blocks/signal/bandpassfilter: add support for normalized nyquist frequency

--- radio/blocks/signal/bandpassfilter.lua
+++ radio/blocks/signal/bandpassfilter.lua
@@ -8,19 +8,23 @@ local FIRFilterBlock = require('radio.blocks.signal.firfilter').FIRFilterBlock
 
 local BandpassFilterBlock = block.factory("BandpassFilterBlock", FIRFilterBlock)
 
-function BandpassFilterBlock:instantiate(num_taps, cutoff_frequencies, window_type)
+function BandpassFilterBlock:instantiate(num_taps, cutoff_frequencies, window_type, nyquist_frequency)
     FIRFilterBlock.instantiate(self, types.Float32Type.vector(num_taps))
 
     self.cutoff_frequencies = cutoff_frequencies
     self.window_type = (window_type == nil) and "hamming" or window_type
+    self.nyquist_frequency = nyquist_frequency
 end
 
 function BandpassFilterBlock:initialize()
     FIRFilterBlock.initialize(self)
 
+    -- Compute Nyquist frequency
+    local nyquist_frequency = self.nyquist_frequency or (self:get_rate()/2)
+
     -- Generate and populate taps
-    self.cutoff_frequencies = {(2*self.cutoff_frequencies[1])/self:get_rate(), (2*self.cutoff_frequencies[2])/self:get_rate()}
-    local real_taps = filter_utils.firwin_bandpass(self.taps.length, self.cutoff_frequencies, self.window_type)
+    local cutoffs = {self.cutoff_frequencies[1]/nyquist_frequency, self.cutoff_frequencies[2]/nyquist_frequency}
+    local real_taps = filter_utils.firwin_bandpass(self.taps.length, cutoffs, self.window_type)
     for i=0, self.taps.length-1 do
         self.taps.data[i].value = real_taps[i+1]
     end
--- tests/generate/generate_block_specs.py
+++ tests/generate/generate_block_specs.py
@@ -290,17 +290,25 @@ def generate_highpassfilter_spec():
 
 @block_spec("BandpassFilterBlock", "tests/blocks/signal/bandpassfilter_spec.lua")
 def generate_bandpassfilter_spec():
-    def process(num_taps, cutoffs, x):
+    def process1(num_taps, cutoffs, x):
         b = scipy.signal.firwin(num_taps, cutoffs, pass_zero=False)
+        return [scipy.signal.lfilter(b, 1, x).astype(type(x[0]))]
+
+    def process2(num_taps, cutoffs, window, nyquist, x):
+        b = scipy.signal.firwin(num_taps, cutoffs, pass_zero=False, window=window.strip('"'), nyq=nyquist)
         return [scipy.signal.lfilter(b, 1, x).astype(type(x[0]))]
 
     vectors = []
     x = random_complex64(256)
-    vectors.append(generate_test_vector(process, [129, [0.1, 0.3]], [x], "129 taps, {0.1, 0.3} cutoff, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
-    vectors.append(generate_test_vector(process, [129, [0.4, 0.6]], [x], "129 taps, {0.4, 0.6} cutoff, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
+    vectors.append(generate_test_vector(process1, [129, [0.1, 0.3]], [x], "129 taps, {0.1, 0.3} cutoff, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
+    vectors.append(generate_test_vector(process1, [129, [0.4, 0.6]], [x], "129 taps, {0.4, 0.6} cutoff, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
+    vectors.append(generate_test_vector(process2, [129, [0.1, 0.3], '"bartlett"', 3.0], [x], "129 taps, {0.1, 0.3} cutoff, bartlett window, 3.0 nyquist, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
+    vectors.append(generate_test_vector(process2, [129, [0.4, 0.6], '"bartlett"', 3.0], [x], "129 taps, {0.4, 0.6} cutoff, bartlett window, 3.0 nyquist, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
     x = random_float32(256)
-    vectors.append(generate_test_vector(process, [129, [0.1, 0.3]], [x], "129 taps, {0.1, 0.3} cutoff, 256 Float32 input, 256 Float32 output"))
-    vectors.append(generate_test_vector(process, [129, [0.4, 0.6]], [x], "129 taps, {0.4, 0.6} cutoff, 256 Float32 input, 256 Float32 output"))
+    vectors.append(generate_test_vector(process1, [129, [0.1, 0.3]], [x], "129 taps, {0.1, 0.3} cutoff, 256 Float32 input, 256 Float32 output"))
+    vectors.append(generate_test_vector(process1, [129, [0.4, 0.6]], [x], "129 taps, {0.4, 0.6} cutoff, 256 Float32 input, 256 Float32 output"))
+    vectors.append(generate_test_vector(process2, [129, [0.1, 0.3], '"bartlett"', 3.0], [x], "129 taps, {0.1, 0.3} cutoff, bartlett window, 3.0 nyquist, 256 Float32 input, 256 ComplexFloat32 output"))
+    vectors.append(generate_test_vector(process2, [129, [0.4, 0.6], '"bartlett"', 3.0], [x], "129 taps, {0.4, 0.6} cutoff, bartlett window, 3.0 nyquist, 256 Float32 input, 256 ComplexFloat32 output"))
 
     return vectors
 

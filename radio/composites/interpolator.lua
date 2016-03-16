ebb629c6aeb4a7d9c7be3c3bb4c10737fd98ab20
Vanya Sergeev <vsergeev@gmail.com>
2016-03-15 22:40:02 -0700

    composites/interpolator: compensate for upsampler attenuation

--- radio/composites/interpolator.lua
+++ radio/composites/interpolator.lua
@@ -2,6 +2,7 @@ local block = require('radio.core.block')
 local types = require('radio.types')
 
 local CompositeBlock = require('radio.core.composite').CompositeBlock
+local MultiplyConstantBlock = require('radio.blocks.signal.multiplyconstant').MultiplyConstantBlock
 local UpsamplerBlock = require('radio.blocks.signal.upsampler').UpsamplerBlock
 local LowpassFilterBlock = require('radio.blocks.signal.lowpassfilter').LowpassFilterBlock
 
@@ -14,11 +15,12 @@ function InterpolatorBlock:instantiate(bandwidth, interpolation, options)
     self:add_type_signature({block.Input("in", types.ComplexFloat32Type)}, {block.Output("out", types.ComplexFloat32Type)})
     self:add_type_signature({block.Input("in", types.Float32Type)}, {block.Output("out", types.Float32Type)})
 
+    local scaler = MultiplyConstantBlock(interpolation)
     local upsampler = UpsamplerBlock(interpolation)
     local filter = LowpassFilterBlock(options.num_taps or 128, bandwidth)
 
-    self:connect(self, "in", upsampler, "in")
-    self:connect(upsampler, filter)
+    self:connect(self, "in", scaler, "in")
+    self:connect(scaler, upsampler, filter)
     self:connect(filter, "out", self, "out")
 end
 

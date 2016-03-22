2389c1eacf8f178b61d126892bee178f44d5208e
Vanya Sergeev <vsergeev@gmail.com>
2016-03-21 21:12:38 -0700

    demos/rtlsdr_ssb: use complex bandpass filter

--- demos/rtlsdr_ssb.lua
+++ demos/rtlsdr_ssb.lua
@@ -9,25 +9,23 @@ assert(arg[2] == "usb" or arg[2] == "lsb", "Sideband should be 'lsb' or 'usb'.")
 
 local frequency = tonumber(arg[1])
 local sideband = arg[2]
-local ifreq = 50e3
-local bandwidth = 3e3
 local gain = tonumber(arg[3]) or 1.0
+local tune_offset = -100e3
+local bandwidth = 3e3
 
 local top = radio.CompositeBlock()
-local source = radio.RtlSdrSource(frequency - ifreq, 1102500)
-local rf_decimator = radio.DecimatorBlock(5)
-local sb_filter = radio.BandpassFilterBlock(257, (sideband == "lsb") and {ifreq - bandwidth, ifreq} or {ifreq, ifreq + bandwidth})
-local translator = radio.FrequencyTranslatorBlock(-ifreq)
+local source = radio.RtlSdrSource(frequency + tune_offset, 1102500)
+local tuner = radio.TunerBlock(tune_offset, 2*bandwidth, 50)
+local sb_filter = radio.ComplexBandpassFilterBlock(257, (sideband == "lsb") and {0, -bandwidth} or {0, bandwidth})
 local am_demod = radio.ComplexToRealBlock()
 local af_gain = radio.MultiplyConstantBlock(gain)
 local af_filter = radio.LowpassFilterBlock(256, bandwidth)
-local af_decimator = radio.DecimatorBlock(10)
 local sink = radio.PulseAudioSink()
 
-local plot1 = radio.GnuplotSpectrumSink(2048, 'RF Spectrum', {xrange = {ifreq - 6*bandwidth, ifreq + 6*bandwidth}, yrange = {-120, -40}})
+local plot1 = radio.GnuplotSpectrumSink(2048, 'RF Spectrum', {xrange = {-6*bandwidth, 6*bandwidth}, yrange = {-120, -40}})
 local plot2 = radio.GnuplotSpectrumSink(2048, 'AF Spectrum', {yrange = {-160, -40}, xrange = {0, bandwidth}, update_time = 0.05})
 
-top:connect(source, rf_decimator, sb_filter, translator, am_demod, af_gain, af_filter, af_decimator, sink)
-top:connect(rf_decimator, plot1)
-top:connect(af_decimator, plot2)
+top:connect(source, tuner, sb_filter, am_demod, af_gain, af_filter, sink)
+top:connect(tuner, plot1)
+top:connect(af_filter, plot2)
 top:run()

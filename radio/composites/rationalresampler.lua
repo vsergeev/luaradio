382ed42604fc057ca0687b0ecfb482148c82a88f
Vanya Sergeev <vsergeev@gmail.com>
2016-04-01 19:51:56 -0700

    composites/rationalresampler: use blocks import

--- radio/composites/rationalresampler.lua
+++ radio/composites/rationalresampler.lua
@@ -1,25 +1,20 @@
 local block = require('radio.core.block')
 local types = require('radio.types')
+local blocks = require('radio.blocks')
 
-local CompositeBlock = require('radio.core.composite').CompositeBlock
-local MultiplyConstantBlock = require('radio.blocks.signal.multiplyconstant').MultiplyConstantBlock
-local UpsamplerBlock = require('radio.blocks.signal.upsampler').UpsamplerBlock
-local LowpassFilterBlock = require('radio.blocks.signal.lowpassfilter').LowpassFilterBlock
-local DownsamplerBlock = require('radio.blocks.signal.downsampler').DownsamplerBlock
-
-local RationalResamplerBlock = block.factory("RationalResamplerBlock", CompositeBlock)
+local RationalResamplerBlock = block.factory("RationalResamplerBlock", blocks.CompositeBlock)
 
 function RationalResamplerBlock:instantiate(interpolation, decimation, options)
-    CompositeBlock.instantiate(self)
+    blocks.CompositeBlock.instantiate(self)
 
     options = options or {}
 
     local cutoff = (1/interpolation < 1/decimation) and 1/interpolation or 1/decimation
 
-    local scaler = MultiplyConstantBlock(interpolation)
-    local upsampler = UpsamplerBlock(interpolation)
-    local filter = LowpassFilterBlock(options.num_taps or 128, cutoff, options.window, 1.0)
-    local downsampler = DownsamplerBlock(decimation)
+    local scaler = blocks.MultiplyConstantBlock(interpolation)
+    local upsampler = blocks.UpsamplerBlock(interpolation)
+    local filter = blocks.LowpassFilterBlock(options.num_taps or 128, cutoff, options.window, 1.0)
+    local downsampler = blocks.DownsamplerBlock(decimation)
     self:connect(scaler, upsampler, filter, downsampler)
 
     self:add_type_signature({block.Input("in", types.ComplexFloat32Type)}, {block.Output("out", types.ComplexFloat32Type)})

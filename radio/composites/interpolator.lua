64573fd21f446cd4ae39598a39098a2c86683779
Vanya Sergeev <vsergeev@gmail.com>
2016-04-01 19:51:44 -0700

    composites/interpolator: use blocks import

--- radio/composites/interpolator.lua
+++ radio/composites/interpolator.lua
@@ -1,21 +1,17 @@
 local block = require('radio.core.block')
 local types = require('radio.types')
+local blocks = require('radio.blocks')
 
-local CompositeBlock = require('radio.core.composite').CompositeBlock
-local MultiplyConstantBlock = require('radio.blocks.signal.multiplyconstant').MultiplyConstantBlock
-local UpsamplerBlock = require('radio.blocks.signal.upsampler').UpsamplerBlock
-local LowpassFilterBlock = require('radio.blocks.signal.lowpassfilter').LowpassFilterBlock
-
-local InterpolatorBlock = block.factory("InterpolatorBlock", CompositeBlock)
+local InterpolatorBlock = block.factory("InterpolatorBlock", blocks.CompositeBlock)
 
 function InterpolatorBlock:instantiate(interpolation, options)
-    CompositeBlock.instantiate(self)
+    blocks.CompositeBlock.instantiate(self)
 
     options = options or {}
 
-    local scaler = MultiplyConstantBlock(interpolation)
-    local upsampler = UpsamplerBlock(interpolation)
-    local filter = LowpassFilterBlock(options.num_taps or 128, 1/interpolation, options.window, 1.0)
+    local scaler = blocks.MultiplyConstantBlock(interpolation)
+    local upsampler = blocks.UpsamplerBlock(interpolation)
+    local filter = blocks.LowpassFilterBlock(options.num_taps or 128, 1/interpolation, options.window, 1.0)
     self:connect(scaler, upsampler, filter)
 
     self:add_type_signature({block.Input("in", types.ComplexFloat32Type)}, {block.Output("out", types.ComplexFloat32Type)})

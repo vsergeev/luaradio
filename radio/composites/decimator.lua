cf5c75980a54e8aded00b18dd795aabc90a2cb05
Vanya Sergeev <vsergeev@gmail.com>
2016-04-01 19:51:33 -0700

    composites/decimator: use blocks import

--- radio/composites/decimator.lua
+++ radio/composites/decimator.lua
@@ -1,19 +1,16 @@
 local block = require('radio.core.block')
 local types = require('radio.types')
+local blocks = require('radio.blocks')
 
-local CompositeBlock = require('radio.core.composite').CompositeBlock
-local LowpassFilterBlock = require('radio.blocks.signal.lowpassfilter').LowpassFilterBlock
-local DownsamplerBlock = require('radio.blocks.signal.downsampler').DownsamplerBlock
-
-local DecimatorBlock = block.factory("DecimatorBlock", CompositeBlock)
+local DecimatorBlock = block.factory("DecimatorBlock", blocks.CompositeBlock)
 
 function DecimatorBlock:instantiate(decimation, options)
-    CompositeBlock.instantiate(self)
+    blocks.CompositeBlock.instantiate(self)
 
     options = options or {}
 
-    local filter = LowpassFilterBlock(options.num_taps or 128, 1/decimation, options.window, 1.0)
-    local downsampler = DownsamplerBlock(decimation)
+    local filter = blocks.LowpassFilterBlock(options.num_taps or 128, 1/decimation, options.window, 1.0)
+    local downsampler = blocks.DownsamplerBlock(decimation)
     self:connect(filter, downsampler)
 
     self:add_type_signature({block.Input("in", types.ComplexFloat32Type)}, {block.Output("out", types.ComplexFloat32Type)})

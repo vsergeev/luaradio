d8c659155f7e47ea1e3539614c56fa910cd98bf2
Vanya Sergeev <vsergeev@gmail.com>
2016-04-07 06:45:55 -0700

    tests/composites/tuner: add TunerBlock unit tests

--- tests/generate/generate_block_specs.py
+++ tests/generate/generate_block_specs.py
@@ -614,6 +614,24 @@ def generate_frequencytranslator_spec():
 
     return vectors
 
+@composite_spec("TunerBlock", "tests/composites/tuner_spec.lua", epsilon=1e-5)
+def generate_tuner_spec():
+    def process(offset, bandwidth, decimation, x):
+        # Rotate
+        x = x * numpy.exp(1j*2*numpy.pi*(offset/2.0)*numpy.arange(len(x))).astype(numpy.complex64)
+        # Filter
+        x = scipy.signal.lfilter(scipy.signal.firwin(128, bandwidth/2), 1, x).astype(x[0])
+        # Downsample
+        x = numpy.array([x[i] for i in range(0, len(x), decimation)])
+        return [x]
+
+    vectors = []
+    x = random_complex64(256)
+    vectors.append(generate_test_vector(process, [0.2, 0.1, 5], [x], "0.2 offset, 0.1 bandwidth, 5 decimation, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
+    vectors.append(generate_test_vector(process, [-0.2, 0.1, 5], [x], "-0.2 offset, 0.1 bandwidth, 5 decimation, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
+
+    return vectors
+
 @block_spec("HilbertTransformBlock", "tests/blocks/signal/hilberttransform_spec.lua")
 def generate_hilberttransform_spec():
     def process(num_taps, x):

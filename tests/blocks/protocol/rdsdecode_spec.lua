db224afc0d0bb245a6c6af52f9f19787312c50b9
Vanya Sergeev <vsergeev@gmail.com>
2016-04-07 06:46:10 -0700

    tests/blocks/protocol/rdsdecode: add RDSDecodeBlock unit tests

---
+++ tests/blocks/protocol/rdsdecode_spec.lua
@@ -0,0 +1,29 @@
+local radio = require('radio')
+local jigs = require('tests.jigs')
+
+jigs.TestBlock(radio.RDSDecodeBlock, {
+    {
+        desc = "Basic Tuning Frame",
+        args = {},
+        inputs = {require('radio.blocks.protocol.rdsframe').RDSFrameType.vector_from_array({{{{0x3aab, 0x02ca, 0xe30a, 0x6963}}}})},
+        outputs = {require('radio.blocks.protocol.rdsdecode').RDSPacketType.vector_from_array({{{pi_code = 15019, tp_code = 0, group_code = 0, group_version = 0, pty_code = 22}, {text_data = 'ic', di_position = 1, text_address = 2, ms_code = 1, di_value = 0, af_code = {227, 10}, type = 'basictuning', ta_code = 0}}})}
+    },
+    {
+        desc = "Radio Text Frame",
+        args = {},
+        inputs = {require('radio.blocks.protocol.rdsframe').RDSFrameType.vector_from_array({{{{0x3aab, 0x22c8, 0x2043, 0x616c}}}})},
+        outputs = {require('radio.blocks.protocol.rdsdecode').RDSPacketType.vector_from_array({{{pi_code = 15019, tp_code = 0, group_code = 2, group_version = 0, pty_code = 22}, {type = 'radiotext', text_data = ' Cal', text_address = 8, ab_flag = 0}}})}
+    },
+    {
+        desc = "Datetime Frame",
+        args = {},
+        inputs = {require('radio.blocks.protocol.rdsframe').RDSFrameType.vector_from_array({{{{0x3aab, 0x42dd, 0xc11a, 0xd0ae}}}})},
+        outputs = {require('radio.blocks.protocol.rdsdecode').RDSPacketType.vector_from_array({{{pi_code = 15019, tp_code = 0, group_code = 4, group_version = 0, pty_code = 22}, {type = 'datetime', time = {offset = -7, hour = 13, minute = 2}, date = {day = 7, year = 2016, month = 4}}}})}
+    },
+    {
+        desc = "Other Frame",
+        args = {},
+        inputs = {require('radio.blocks.protocol.rdsframe').RDSFrameType.vector_from_array({{{{0x3aab, 0x82c0, 0x18ed, 0x14fa}}}})},
+        outputs = {require('radio.blocks.protocol.rdsdecode').RDSPacketType.vector_from_array({{{pi_code = 15019, tp_code = 0, group_code = 8, group_version = 0, pty_code = 22}, {type = 'raw', frame = {15019,33472,6381,5370}}}})}
+    },
+}, {epsilon = 1.0e-06})
--- tests/generate/generate_block_specs.py
+++ tests/generate/generate_block_specs.py
@@ -1115,6 +1115,30 @@ def generate_rdsframe_spec():
 
     return vectors
 
+@block_spec("RDSDecodeBlock", "tests/blocks/protocol/rdsdecode_spec.lua")
+def generate_rdsdecode_spec():
+    def test_vector_wrapper(packets):
+        template = "require('radio.blocks.protocol.rdsdecode').RDSPacketType.vector_from_array({%s})"
+        return lambda x: [template % (','.join(packets))]
+
+    vectors = []
+
+    frame1 = "require('radio.blocks.protocol.rdsframe').RDSFrameType.vector_from_array({{{{0x3aab, 0x02ca, 0xe30a, 0x6963}}}})"
+    packet1 = "{{pi_code = 15019, tp_code = 0, group_code = 0, group_version = 0, pty_code = 22}, {text_data = 'ic', di_position = 1, text_address = 2, ms_code = 1, di_value = 0, af_code = {227, 10}, type = 'basictuning', ta_code = 0}}"
+    frame2 = "require('radio.blocks.protocol.rdsframe').RDSFrameType.vector_from_array({{{{0x3aab, 0x22c8, 0x2043, 0x616c}}}})"
+    packet2 = "{{pi_code = 15019, tp_code = 0, group_code = 2, group_version = 0, pty_code = 22}, {type = 'radiotext', text_data = ' Cal', text_address = 8, ab_flag = 0}}"
+    frame3 = "require('radio.blocks.protocol.rdsframe').RDSFrameType.vector_from_array({{{{0x3aab, 0x42dd, 0xc11a, 0xd0ae}}}})"
+    packet3 = "{{pi_code = 15019, tp_code = 0, group_code = 4, group_version = 0, pty_code = 22}, {type = 'datetime', time = {offset = -7, hour = 13, minute = 2}, date = {day = 7, year = 2016, month = 4}}}"
+    frame4 = "require('radio.blocks.protocol.rdsframe').RDSFrameType.vector_from_array({{{{0x3aab, 0x82c0, 0x18ed, 0x14fa}}}})"
+    packet4 = "{{pi_code = 15019, tp_code = 0, group_code = 8, group_version = 0, pty_code = 22}, {type = 'raw', frame = {15019,33472,6381,5370}}}"
+
+    vectors.append(generate_test_vector(test_vector_wrapper([packet1]), [], [frame1], "Basic Tuning Frame"))
+    vectors.append(generate_test_vector(test_vector_wrapper([packet2]), [], [frame2], "Radio Text Frame"))
+    vectors.append(generate_test_vector(test_vector_wrapper([packet3]), [], [frame3], "Datetime Frame"))
+    vectors.append(generate_test_vector(test_vector_wrapper([packet4]), [], [frame4], "Other Frame"))
+
+    return vectors
+
 ################################################################################
 # Source block test vectors
 ################################################################################

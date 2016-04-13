d0954f620c8a0be3ab3d79ab01bf794bdc11f262
Vanya Sergeev <vsergeev@gmail.com>
2016-04-13 05:40:54 -0700

    tests/blocks/signal/delay: add additional unit tests and fix descriptions

--- tests/generate/blocks/signal/delay_spec.py
+++ tests/generate/blocks/signal/delay_spec.py
@@ -4,24 +4,26 @@ from generate import *
 
 def generate():
     def process(n, x):
-        elem_type = type(x[0])
-        return [numpy.insert(x, 0, [elem_type()] * n)[:len(x)]]
+        return [numpy.insert(x, 0, [type(x[0])()] * n)[:len(x)]]
 
     vectors = []
 
     x = random_complex64(256)
-    vectors.append(TestVector([1], [x], process(1, x), "1 Sample Delay, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
-    vectors.append(TestVector([15], [x], process(15, x), "1 Sample Delay, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
-    vectors.append(TestVector([100], [x], process(100, x), "1 Sample Delay, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
+    vectors.append(TestVector([1], [x], process(1, x), "1 Sample Delay, 256 ComplexFloat32 input, 257 ComplexFloat32 output"))
+    vectors.append(TestVector([15], [x], process(15, x), "15 Sample Delay, 256 ComplexFloat32 input, 271 ComplexFloat32 output"))
+    vectors.append(TestVector([100], [x], process(100, x), "100 Sample Delay, 256 ComplexFloat32 input, 356 ComplexFloat32 output"))
+    vectors.append(TestVector([307], [x], process(307, x), "307 Sample Delay, 256 ComplexFloat32 input, 563 ComplexFloat32 output"))
 
     x = random_float32(256)
-    vectors.append(TestVector([1], [x], process(1, x), "1 Sample Delay, 256 Float32 input, 256 Float32 output"))
-    vectors.append(TestVector([15], [x], process(15, x), "1 Sample Delay, 256 Float32 input, 256 Float32 output"))
-    vectors.append(TestVector([100], [x], process(100, x), "1 Sample Delay, 256 Float32 input, 256 Float32 output"))
+    vectors.append(TestVector([1], [x], process(1, x), "1 Sample Delay, 256 Float32 input, 257 Float32 output"))
+    vectors.append(TestVector([15], [x], process(15, x), "15 Sample Delay, 256 Float32 input, 271 Float32 output"))
+    vectors.append(TestVector([100], [x], process(100, x), "100 Sample Delay, 256 Float32 input, 356 Float32 output"))
+    vectors.append(TestVector([307], [x], process(307, x), "307 Sample Delay, 256 Float32 input, 563 Float32 output"))
 
     x = random_integer32(256)
-    vectors.append(TestVector([1], [x], process(1, x), "1 Sample Delay, 256 Integer32 input, 256 Integer32 output"))
-    vectors.append(TestVector([15], [x], process(15, x), "1 Sample Delay, 256 Integer32 input, 256 Integer32 output"))
-    vectors.append(TestVector([100], [x], process(100, x), "1 Sample Delay, 256 Integer32 input, 256 Integer32 output"))
+    vectors.append(TestVector([1], [x], process(1, x), "1 Sample Delay, 256 Integer32 input, 257 Integer32 output"))
+    vectors.append(TestVector([15], [x], process(15, x), "15 Sample Delay, 256 Integer32 input, 271 Integer32 output"))
+    vectors.append(TestVector([100], [x], process(100, x), "100 Sample Delay, 256 Integer32 input, 356 Integer32 output"))
+    vectors.append(TestVector([307], [x], process(307, x), "307 Sample Delay, 256 Integer32 input, 563 Integer32 output"))
 
     return BlockSpec("DelayBlock", "tests/blocks/signal/delay_spec.lua", vectors, 1e-6)

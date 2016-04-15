79cbc24953434ff6ea485c389f7fb7bf1fe558ca
Vanya Sergeev <vsergeev@gmail.com>
2016-04-15 03:50:48 -0700

    tests/blocks/signal/iirfilter: fix unit test descriptions

--- tests/generate/blocks/signal/iirfilter_spec.py
+++ tests/generate/blocks/signal/iirfilter_spec.py
@@ -25,8 +25,8 @@ def generate():
     b_taps, a_taps = gentaps(3)
     vectors.append(TestVector([b_taps, a_taps], [x], process(b_taps, a_taps, x), "3 Float32 b taps, 3 Float32 a taps, 256 Float32 input, 256 Float32 output"))
     b_taps, a_taps = gentaps(5)
-    vectors.append(TestVector([b_taps, a_taps], [x], process(b_taps, a_taps, x), "5 Float32 b taps, 5 Float32 a taps, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
+    vectors.append(TestVector([b_taps, a_taps], [x], process(b_taps, a_taps, x), "5 Float32 b taps, 5 Float32 a taps, 256 Float32 input, 256 Float32 output"))
     b_taps, a_taps = gentaps(10)
-    vectors.append(TestVector([b_taps, a_taps], [x], process(b_taps, a_taps, x), "10 Float32 b taps, 10 Float32 a taps, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
+    vectors.append(TestVector([b_taps, a_taps], [x], process(b_taps, a_taps, x), "10 Float32 b taps, 10 Float32 a taps, 256 Float32 input, 256 Float32 output"))
 
     return BlockSpec("IIRFilterBlock", "tests/blocks/signal/iirfilter_spec.lua", vectors, 1e-6)

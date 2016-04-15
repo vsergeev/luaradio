f2eb9f25b03bcb3963e413243f573b3817ce1b98
Vanya Sergeev <vsergeev@gmail.com>
2016-04-15 01:22:47 -0700

    blocks/signal/rootraisedcosinefilter: initialize base class after setting taps

--- radio/blocks/signal/rootraisedcosinefilter.lua
+++ radio/blocks/signal/rootraisedcosinefilter.lua
@@ -16,13 +16,13 @@ function RootRaisedCosineFilterBlock:instantiate(num_taps, beta, symbol_rate)
 end
 
 function RootRaisedCosineFilterBlock:initialize()
-    FIRFilterBlock.initialize(self)
-
     -- Generate and populate taps
     local real_taps = filter_utils.fir_root_raised_cosine(self.taps.length, self:get_rate(), self.beta, 1/self.symbol_rate)
     for i=0, self.taps.length-1 do
         self.taps.data[i].value = real_taps[i+1]
     end
+
+    FIRFilterBlock.initialize(self)
 end
 
 return {RootRaisedCosineFilterBlock = RootRaisedCosineFilterBlock}

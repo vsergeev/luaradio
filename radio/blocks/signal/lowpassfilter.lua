ff9e050fa4ad2de4e592a60cb4d945d40daa52f3
Vanya Sergeev <vsergeev@gmail.com>
2016-04-15 01:21:05 -0700

    blocks/signal/lowpassfilter: initialize base class after setting taps

--- radio/blocks/signal/lowpassfilter.lua
+++ radio/blocks/signal/lowpassfilter.lua
@@ -17,8 +17,6 @@ function LowpassFilterBlock:instantiate(num_taps, cutoff_frequency, window_type,
 end
 
 function LowpassFilterBlock:initialize()
-    FIRFilterBlock.initialize(self)
-
     -- Compute Nyquist frequency
     local nyquist_frequency = self.nyquist_frequency or (self:get_rate()/2)
 
@@ -27,6 +25,8 @@ function LowpassFilterBlock:initialize()
     for i=0, self.taps.length-1 do
         self.taps.data[i].value = real_taps[i+1]
     end
+
+    FIRFilterBlock.initialize(self)
 end
 
 return {LowpassFilterBlock = LowpassFilterBlock}

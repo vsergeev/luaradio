5fdae13551b3a1fcadd319f61d2c1282ba258eb6
Vanya Sergeev <vsergeev@gmail.com>
2016-04-15 01:21:35 -0700

    blocks/signal/highpassfilter: initialize base class after setting taps

--- radio/blocks/signal/highpassfilter.lua
+++ radio/blocks/signal/highpassfilter.lua
@@ -17,8 +17,6 @@ function HighpassFilterBlock:instantiate(num_taps, cutoff_frequency, window_type
 end
 
 function HighpassFilterBlock:initialize()
-    FIRFilterBlock.initialize(self)
-
     -- Compute Nyquist frequency
     local nyquist_frequency = self.nyquist_frequency or (self:get_rate()/2)
 
@@ -27,6 +25,8 @@ function HighpassFilterBlock:initialize()
     for i=0, self.taps.length-1 do
         self.taps.data[i].value = real_taps[i+1]
     end
+
+    FIRFilterBlock.initialize(self)
 end
 
 return {HighpassFilterBlock = HighpassFilterBlock}

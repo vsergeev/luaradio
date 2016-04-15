269bc654c406a7ab13b72674b8cf3ca95866431b
Vanya Sergeev <vsergeev@gmail.com>
2016-04-15 01:21:51 -0700

    blocks/signal/bandpassfilter: initialize base class after setting taps

--- radio/blocks/signal/bandpassfilter.lua
+++ radio/blocks/signal/bandpassfilter.lua
@@ -17,8 +17,6 @@ function BandpassFilterBlock:instantiate(num_taps, cutoff_frequencies, window_ty
 end
 
 function BandpassFilterBlock:initialize()
-    FIRFilterBlock.initialize(self)
-
     -- Compute Nyquist frequency
     local nyquist_frequency = self.nyquist_frequency or (self:get_rate()/2)
 
@@ -28,6 +26,8 @@ function BandpassFilterBlock:initialize()
     for i=0, self.taps.length-1 do
         self.taps.data[i].value = real_taps[i+1]
     end
+
+    FIRFilterBlock.initialize(self)
 end
 
 return {BandpassFilterBlock = BandpassFilterBlock}

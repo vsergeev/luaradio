5659555db5a3b880262bd6a4b66b83c92a1ca9cb
Vanya Sergeev <vsergeev@gmail.com>
2016-04-15 20:32:16 -0700

    radio/init: update package version for v0.0.14

--- radio/init.lua
+++ radio/init.lua
@@ -2,9 +2,9 @@ assert(pcall(require, 'ffi') and pcall(require, 'jit'), 'Error: LuaRadio require
 
 local radio = {
     -- Version
-    _VERSION = "0.0.13",
-    version = "0.0.13",
-    version_number = 000013,
+    _VERSION = "0.0.14",
+    version = "0.0.14",
+    version_number = 000014,
 
     -- Core modules
     platform = require('radio.core.platform'),

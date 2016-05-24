1269b2b9801439c08222bc36500c08244bfcae75
Vanya Sergeev <vsergeev@gmail.com>
2016-05-24 06:35:42 -0700

    composites/amsynchronousdemodulator: remove constant audio gain block

--- radio/composites/amsynchronousdemodulator.lua
+++ radio/composites/amsynchronousdemodulator.lua
@@ -4,22 +4,20 @@ local blocks = require('radio.blocks')
 
 local AMSynchronousDemodulator = block.factory("AMSynchronousDemodulator", blocks.CompositeBlock)
 
-function AMSynchronousDemodulator:instantiate(ifreq, bandwidth, gain)
+function AMSynchronousDemodulator:instantiate(ifreq, bandwidth)
     blocks.CompositeBlock.instantiate(self)
 
     bandwidth = bandwidth or 5e3
-    gain = gain or 1.0
 
     local rf_filter = blocks.ComplexBandpassFilterBlock(129, {ifreq - bandwidth, ifreq + bandwidth})
     local pll = blocks.PLLBlock(1000, ifreq - 100, ifreq + 100)
     local mixer = blocks.MultiplyConjugateBlock()
     local am_demod = blocks.ComplexToRealBlock()
-    local af_gain = blocks.MultiplyConstantBlock(gain)
     local af_filter = blocks.LowpassFilterBlock(128, bandwidth)
     self:connect(rf_filter, pll)
     self:connect(rf_filter, 'out', mixer, 'in1')
     self:connect(pll, 'out', mixer, 'in2')
-    self:connect(mixer, am_demod, af_gain, af_filter)
+    self:connect(mixer, am_demod, af_filter)
 
     self:add_type_signature({block.Input("in", types.ComplexFloat32)}, {block.Output("out", types.Float32)})
     self:connect(self, "in", rf_filter, "in")

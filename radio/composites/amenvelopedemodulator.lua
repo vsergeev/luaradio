fad0421094a56ad0ec2fd446a06bf25855735dfe
Vanya Sergeev <vsergeev@gmail.com>
2016-05-24 06:35:14 -0700

    composites/amenvelopedemodulator: remove constant audio gain block

--- radio/composites/amenvelopedemodulator.lua
+++ radio/composites/amenvelopedemodulator.lua
@@ -4,16 +4,14 @@ local blocks = require('radio.blocks')
 
 local AMEnvelopeDemodulator = block.factory("AMEnvelopeDemodulator", blocks.CompositeBlock)
 
-function AMEnvelopeDemodulator:instantiate(bandwidth, gain)
+function AMEnvelopeDemodulator:instantiate(bandwidth)
     blocks.CompositeBlock.instantiate(self)
 
     bandwidth = bandwidth or 5e3
-    gain = gain or 1.0
 
     local am_demod = blocks.ComplexMagnitudeBlock()
-    local af_gain = blocks.MultiplyConstantBlock(gain)
     local af_filter = blocks.LowpassFilterBlock(128, bandwidth)
-    self:connect(am_demod, af_gain, af_filter)
+    self:connect(am_demod, af_filter)
 
     self:add_type_signature({block.Input("in", types.ComplexFloat32)}, {block.Output("out", types.Float32)})
     self:connect(self, "in", am_demod, "in")

0aa51f0e01b8dbbd97dff57af471fcefa469e90b
Vanya Sergeev <vsergeev@gmail.com>
2016-05-24 06:35:58 -0700

    composites/ssbdemodulator: remove constant audio gain block

--- radio/composites/ssbdemodulator.lua
+++ radio/composites/ssbdemodulator.lua
@@ -4,18 +4,16 @@ local blocks = require('radio.blocks')
 
 local SSBDemodulator = block.factory("SSBDemodulator", blocks.CompositeBlock)
 
-function SSBDemodulator:instantiate(sideband, bandwidth, gain)
+function SSBDemodulator:instantiate(sideband, bandwidth)
     blocks.CompositeBlock.instantiate(self)
 
     assert(sideband == "lsb" or sideband == "usb", "Sideband should be 'lsb' or 'usb'.")
     bandwidth = bandwidth or 3e3
-    gain = gain or 1.0
 
     local sb_filter = blocks.ComplexBandpassFilterBlock(129, (sideband == "lsb") and {0, -bandwidth} or {0, bandwidth})
     local am_demod = blocks.ComplexToRealBlock()
-    local af_gain = blocks.MultiplyConstantBlock(gain)
     local af_filter = blocks.LowpassFilterBlock(128, bandwidth)
-    self:connect(sb_filter, am_demod, af_gain, af_filter)
+    self:connect(sb_filter, am_demod, af_filter)
 
     self:add_type_signature({block.Input("in", types.ComplexFloat32)}, {block.Output("out", types.Float32)})
     self:connect(self, "in", sb_filter, "in")

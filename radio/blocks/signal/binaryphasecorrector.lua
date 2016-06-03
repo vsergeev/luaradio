2923955cce3428368bdf66c0254ee84335ee9a6d
Vanya Sergeev <vsergeev@gmail.com>
2016-06-03 04:10:34 -0700

    blocks/signal/binaryphasecorrector: add sampling interval to reduce computation

--- radio/blocks/signal/binaryphasecorrector.lua
+++ radio/blocks/signal/binaryphasecorrector.lua
@@ -7,10 +7,13 @@ local types = require('radio.types')
 
 local BinaryPhaseCorrectorBlock = block.factory("BinaryPhaseCorrectorBlock")
 
-function BinaryPhaseCorrectorBlock:instantiate(num_samples)
+function BinaryPhaseCorrectorBlock:instantiate(num_samples, sample_interval)
     self.num_samples = num_samples
-    self.phi_moving_average = 0.0
+    self.sample_interval = sample_interval or 32
+
+    self.sample_index = 0
     self.phi_state = types.Float32.vector(self.num_samples)
+    self.phi_moving_average = 0.0
 
     self:add_type_signature({block.Input("in", types.ComplexFloat32)}, {block.Output("out", types.ComplexFloat32)})
 end
@@ -23,9 +26,10 @@ function BinaryPhaseCorrectorBlock:process(x)
     local out = types.ComplexFloat32.vector(x.length)
 
     for i = 0, x.length-1 do
-        if x.data[i].real ~= 0 then
+        if i == self.sample_index then
             -- Calculate angle of this sample
             local phi = x.data[i]:arg()
+
             -- Clamp the angle to the right quadrants
             phi = (phi < -math.pi/2) and (phi + math.pi) or phi
             phi = (phi > math.pi/2) and (phi - math.pi) or phi
@@ -39,10 +43,18 @@ function BinaryPhaseCorrectorBlock:process(x)
 
             -- Update the moving average
             self.phi_moving_average = self.phi_moving_average + phi/self.num_samples - last_phi/self.num_samples
+
+            -- Update sample index
+            self.sample_index = self.sample_index + self.sample_interval
         end
 
         -- Adjust the phase of this sample with the moving average
         out.data[i] = x.data[i] * types.ComplexFloat32(math.cos(-self.phi_moving_average), math.sin(-self.phi_moving_average))
+    end
+
+    -- Wrap sample index
+    if self.sample_index >= x.length then
+        self.sample_index = self.sample_index - x.length
     end
 
     return out
--- tests/generate/blocks/signal/binaryphasecorrector_spec.py
+++ tests/generate/blocks/signal/binaryphasecorrector_spec.py
@@ -3,16 +3,17 @@ from generate import *
 
 
 def generate():
-    def process(num_samples, x):
+    def process(num_samples, sample_interval, x):
         phi_state = [0.0] * num_samples
         out = []
 
-        for e in x:
-            phi = numpy.arctan2(e.imag, e.real)
-            phi = (phi + numpy.pi) if phi < -numpy.pi / 2 else phi
-            phi = (phi - numpy.pi) if phi > numpy.pi / 2 else phi
-            phi_state = phi_state[1:] + [phi]
-            phi_avg = numpy.mean(phi_state)
+        for i, e in enumerate(x):
+            if (i % sample_interval) == 0:
+                phi = numpy.arctan2(e.imag, e.real)
+                phi = (phi + numpy.pi) if phi < -numpy.pi / 2 else phi
+                phi = (phi - numpy.pi) if phi > numpy.pi / 2 else phi
+                phi_state = phi_state[1:] + [phi]
+                phi_avg = numpy.mean(phi_state)
 
             out.append(e * numpy.complex64(complex(numpy.cos(-phi_avg), numpy.sin(-phi_avg))))
 
@@ -21,9 +22,9 @@ def generate():
     vectors = []
 
     x = random_complex64(256)
-    vectors.append(TestVector([4], [x], process(4, x), "4 sample average, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
-    vectors.append(TestVector([17], [x], process(17, x), "17 sample average, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
-    vectors.append(TestVector([64], [x], process(64, x), "64 sample average, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
-    vectors.append(TestVector([100], [x], process(100, x), "100 sample average, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
+    vectors.append(TestVector([4, 1], [x], process(4, 1, x), "4 sample average, 1 sample interval, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
+    vectors.append(TestVector([17, 15], [x], process(17, 15, x), "17 sample average, 15 sample interval, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
+    vectors.append(TestVector([64, 7], [x], process(64, 7, x), "64 sample average, 7 sample interval, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
+    vectors.append(TestVector([100, 32], [x], process(100, 32, x), "100 sample average, 32 sample interval, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
 
     return BlockSpec("BinaryPhaseCorrectorBlock", "tests/blocks/signal/binaryphasecorrector_spec.lua", vectors, 1e-6)

1493eba7c0a843110b3d35eb7178a30e4fc644a3
Vanya Sergeev <vsergeev@gmail.com>
2016-06-20 06:50:17 -0700

    examples/rtlsdr_wbfm_mono: remove bandwidth variable

--- examples/rtlsdr_wbfm_mono.lua
+++ examples/rtlsdr_wbfm_mono.lua
@@ -7,19 +7,18 @@ end
 
 local frequency = tonumber(arg[1])
 local tune_offset = -250e3
-local bandwidth = 15e3
 
 local top = radio.CompositeBlock()
 local source = radio.RtlSdrSource(frequency + tune_offset, 1102500)
 local tuner = radio.TunerBlock(tune_offset, 200e3, 5)
 local fm_demod = radio.FrequencyDiscriminatorBlock(1.25)
-local af_filter = radio.LowpassFilterBlock(128, bandwidth)
+local af_filter = radio.LowpassFilterBlock(128, 15e3)
 local af_deemphasis = radio.FMDeemphasisFilterBlock(75e-6)
 local af_downsampler = radio.DownsamplerBlock(5)
 local sink = os.getenv('DISPLAY') and radio.PulseAudioSink(1) or radio.WAVFileSink('wbfm_mono.wav', 1)
 
 local plot1 = radio.GnuplotSpectrumSink(2048, 'Demodulated FM Spectrum', {yrange = {-120, -40}})
-local plot2 = radio.GnuplotSpectrumSink(2048, 'L+R AF Spectrum', {yrange = {-120, -40}, xrange = {0, bandwidth}, update_time = 0.05})
+local plot2 = radio.GnuplotSpectrumSink(2048, 'L+R AF Spectrum', {yrange = {-120, -40}, xrange = {0, 15e3}, update_time = 0.05})
 
 top:connect(source, tuner, fm_demod, af_filter, af_deemphasis, af_downsampler, sink)
 if os.getenv('DISPLAY') then

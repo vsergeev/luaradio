e43ee306dd04b5c834cf6a8f6c75240abd38c459
Vanya Sergeev <vsergeev@gmail.com>
2016-06-20 06:48:47 -0700

    examples/rtlsdr_wbfm_mono: rename decimator var name to downsampler

--- examples/rtlsdr_wbfm_mono.lua
+++ examples/rtlsdr_wbfm_mono.lua
@@ -15,13 +15,13 @@ local tuner = radio.TunerBlock(tune_offset, 200e3, 5)
 local fm_demod = radio.FrequencyDiscriminatorBlock(1.25)
 local af_filter = radio.LowpassFilterBlock(128, bandwidth)
 local af_deemphasis = radio.FMDeemphasisFilterBlock(75e-6)
-local af_decimator = radio.DownsamplerBlock(5)
+local af_downsampler = radio.DownsamplerBlock(5)
 local sink = os.getenv('DISPLAY') and radio.PulseAudioSink(1) or radio.WAVFileSink('wbfm_mono.wav', 1)
 
 local plot1 = radio.GnuplotSpectrumSink(2048, 'Demodulated FM Spectrum', {yrange = {-120, -40}})
 local plot2 = radio.GnuplotSpectrumSink(2048, 'L+R AF Spectrum', {yrange = {-120, -40}, xrange = {0, bandwidth}, update_time = 0.05})
 
-top:connect(source, tuner, fm_demod, af_filter, af_deemphasis, af_decimator, sink)
+top:connect(source, tuner, fm_demod, af_filter, af_deemphasis, af_downsampler, sink)
 if os.getenv('DISPLAY') then
     top:connect(fm_demod, plot1)
     top:connect(af_deemphasis, plot2)

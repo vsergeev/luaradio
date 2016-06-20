a917359351882947982e732f91b09d6a61d8e28a
Vanya Sergeev <vsergeev@gmail.com>
2016-06-20 06:56:13 -0700

    examples/wavfile_ssb_modulator: add file path variables for clarity

--- examples/wavfile_ssb_modulator.lua
+++ examples/wavfile_ssb_modulator.lua
@@ -7,16 +7,18 @@ end
 
 assert(arg[4] == "usb" or arg[4] == "lsb", "Sideband should be 'lsb' or 'usb'.")
 
+local wav_filepath = arg[1]
+local iq_filepath = arg[2]
 local bandwidth = tonumber(arg[3])
 local sideband = arg[4]
 
 local top = radio.CompositeBlock()
-local source = radio.WAVFileSource(arg[1], 1)
+local source = radio.WAVFileSource(wav_filepath, 1)
 local af_filter = radio.LowpassFilterBlock(128, bandwidth)
 local hilbert = radio.HilbertTransformBlock(129)
 local conjugate = radio.ComplexConjugateBlock()
 local sb_filter = radio.ComplexBandpassFilterBlock(129, (sideband == "lsb") and {-bandwidth, 0} or {0, bandwidth})
-local sink = radio.IQFileSink(arg[2], 'f32le')
+local sink = radio.IQFileSink(iq_filepath, 'f32le')
 
 if sideband == "lsb" then
     top:connect(source, af_filter, hilbert, conjugate, sb_filter, sink)

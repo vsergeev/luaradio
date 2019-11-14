25c27aeea2835763acc370cbc811e4f3787835b3
Vanya A. Sergeev <v@sergeev.io>
2019-11-13 23:55:54 -0600

    blocks/sinks/realfile: refactor to use format_utils

--- radio/blocks/sinks/realfile.lua
+++ radio/blocks/sinks/realfile.lua
@@ -28,43 +28,9 @@ local ffi = require('ffi')
 
 local block = require('radio.core.block')
 local types = require('radio.types')
+local format_utils = require('radio.blocks.sources.format_utils')
 
 local RealFileSink = block.factory("RealFileSink")
-
--- Real Formats
-ffi.cdef[[
-    typedef struct {
-        union { uint8_t bytes[1]; uint8_t value; };
-    } format_u8_t;
-
-    typedef struct {
-        union { uint8_t bytes[1]; int8_t value; };
-    } format_s8_t;
-
-    typedef struct {
-        union { uint8_t bytes[2]; uint16_t value; };
-    } format_u16_t;
-
-    typedef struct {
-        union { uint8_t bytes[2]; int16_t value; };
-    } format_s16_t;
-
-    typedef struct {
-        union { uint8_t bytes[4]; uint32_t value; };
-    } format_u32_t;
-
-    typedef struct {
-        union { uint8_t bytes[4]; int32_t value; };
-    } format_s32_t;
-
-    typedef struct {
-        union { uint8_t bytes[4]; float value; };
-    } format_f32_t;
-
-    typedef struct {
-        union { uint8_t bytes[8]; double value; };
-    } format_f64_t;
-]]
 
 -- File I/O
 ffi.cdef[[
@@ -77,23 +43,6 @@ ffi.cdef[[
 ]]
 
 function RealFileSink:instantiate(file, format)
-    local supported_formats = {
-        u8    = {ctype = "format_u8_t",  swap = false,         offset = 127.5,         scale = 127.5},
-        s8    = {ctype = "format_s8_t",  swap = false,         offset = 0,             scale = 127.5},
-        u16le = {ctype = "format_u16_t", swap = ffi.abi("be"), offset = 32767.5,       scale = 32767.5},
-        u16be = {ctype = "format_u16_t", swap = ffi.abi("le"), offset = 32767.5,       scale = 32767.5},
-        s16le = {ctype = "format_s16_t", swap = ffi.abi("be"), offset = 0,             scale = 32767.5},
-        s16be = {ctype = "format_s16_t", swap = ffi.abi("le"), offset = 0,             scale = 32767.5},
-        u32le = {ctype = "format_u32_t", swap = ffi.abi("be"), offset = 2147483647.5,  scale = 2147483647.5},
-        u32be = {ctype = "format_u32_t", swap = ffi.abi("le"), offset = 2147483647.5,  scale = 2147483647.5},
-        s32le = {ctype = "format_s32_t", swap = ffi.abi("be"), offset = 0,             scale = 2147483647.5},
-        s32be = {ctype = "format_s32_t", swap = ffi.abi("le"), offset = 0,             scale = 2147483647.5},
-        f32le = {ctype = "format_f32_t", swap = ffi.abi("be"), offset = 0,             scale = 1.0},
-        f32be = {ctype = "format_f32_t", swap = ffi.abi("le"), offset = 0,             scale = 1.0},
-        f64le = {ctype = "format_f64_t", swap = ffi.abi("be"), offset = 0,             scale = 1.0},
-        f64be = {ctype = "format_f64_t", swap = ffi.abi("le"), offset = 0,             scale = 1.0},
-    }
-
     if type(file) == "string" then
         self.filename = file
     elseif type(file) == "number" then
@@ -102,7 +51,7 @@ function RealFileSink:instantiate(file, format)
         self.file = assert(file, "Missing argument #1 (file)")
     end
 
-    self.format = assert(supported_formats[format], "Unsupported format (\"" .. format .. "\")")
+    self.format = assert(format_utils.formats[format], "Unsupported format (\"" .. format .. "\")")
 
     self:add_type_signature({block.Input("in", types.Float32)}, {})
 end
@@ -124,17 +73,9 @@ function RealFileSink:initialize()
     self.files[self.file] = true
 end
 
-local function swap_bytes(x)
-    local len = ffi.sizeof(x.bytes)
-
-    for i = 0, (len/2)-1 do
-        x.bytes[i], x.bytes[len-i-1] = x.bytes[len-i-1], x.bytes[i]
-    end
-end
-
 function RealFileSink:process(x)
     -- Allocate buffer for binary samples
-    local raw_samples = ffi.new(self.format.ctype .. "[?]", x.length)
+    local raw_samples = ffi.new(ffi.typeof("$ [?]", self.format.real_ctype), x.length)
 
     -- Convert Float32 samples to raw samples
     for i = 0, x.length-1 do
@@ -144,12 +85,12 @@ function RealFileSink:process(x)
     -- Perform byte swap for endianness if needed
     if self.format.swap then
         for i = 0, x.length-1 do
-            swap_bytes(raw_samples[i])
+            format_utils.swap_bytes(raw_samples[i])
         end
     end
 
     -- Write to file
-    local num_samples = ffi.C.fwrite(raw_samples, ffi.sizeof(self.format.ctype), x.length, self.file)
+    local num_samples = ffi.C.fwrite(raw_samples, ffi.sizeof(self.format.real_ctype), x.length, self.file)
     if num_samples ~= x.length then
         error("fwrite(): " .. ffi.string(ffi.C.strerror(ffi.errno())))
     end

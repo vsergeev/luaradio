25578d278980feb9768d657af18a4a7c71336286
Vanya A. Sergeev <v@sergeev.io>
2019-11-13 23:54:37 -0600

    blocks/sources/iqfile: refactor to use format_utils

--- radio/blocks/sources/iqfile.lua
+++ radio/blocks/sources/iqfile.lua
@@ -31,51 +31,9 @@ local ffi = require('ffi')
 local block = require('radio.core.block')
 local vector = require('radio.core.vector')
 local types = require('radio.types')
+local format_utils = require('radio.blocks.sources.format_utils')
 
 local IQFileSource = block.factory("IQFileSource")
-
--- IQ Formats
-ffi.cdef[[
-    typedef struct {
-        union { uint8_t bytes[1]; uint8_t value; } real;
-        union { uint8_t bytes[1]; uint8_t value; } imag;
-    } iq_format_u8_t;
-
-    typedef struct {
-        union { uint8_t bytes[1]; int8_t value; } real;
-        union { uint8_t bytes[1]; int8_t value; } imag;
-    } iq_format_s8_t;
-
-    typedef struct {
-        union { uint8_t bytes[2]; uint16_t value; } real;
-        union { uint8_t bytes[2]; uint16_t value; } imag;
-    } iq_format_u16_t;
-
-    typedef struct {
-        union { uint8_t bytes[2]; int16_t value; } real;
-        union { uint8_t bytes[2]; int16_t value; } imag;
-    } iq_format_s16_t;
-
-    typedef struct {
-        union { uint8_t bytes[4]; uint32_t value; } real;
-        union { uint8_t bytes[4]; uint32_t value; } imag;
-    } iq_format_u32_t;
-
-    typedef struct {
-        union { uint8_t bytes[4]; int32_t value; } real;
-        union { uint8_t bytes[4]; int32_t value; } imag;
-    } iq_format_s32_t;
-
-    typedef struct {
-        union { uint8_t bytes[4]; float value; } real;
-        union { uint8_t bytes[4]; float value; } imag;
-    } iq_format_f32_t;
-
-    typedef struct {
-        union { uint8_t bytes[8]; double value; } real;
-        union { uint8_t bytes[8]; double value; } imag;
-    } iq_format_f64_t;
-]]
 
 -- File I/O
 ffi.cdef[[
@@ -90,23 +48,6 @@ ffi.cdef[[
 ]]
 
 function IQFileSource:instantiate(file, format, rate, repeat_on_eof)
-    local supported_formats = {
-        u8    = {ctype = "iq_format_u8_t",  swap = false,         offset = 127.5,         scale = 1.0/127.5},
-        s8    = {ctype = "iq_format_s8_t",  swap = false,         offset = 0,             scale = 1.0/127.5},
-        u16le = {ctype = "iq_format_u16_t", swap = ffi.abi("be"), offset = 32767.5,       scale = 1.0/32767.5},
-        u16be = {ctype = "iq_format_u16_t", swap = ffi.abi("le"), offset = 32767.5,       scale = 1.0/32767.5},
-        s16le = {ctype = "iq_format_s16_t", swap = ffi.abi("be"), offset = 0,             scale = 1.0/32767.5},
-        s16be = {ctype = "iq_format_s16_t", swap = ffi.abi("le"), offset = 0,             scale = 1.0/32767.5},
-        u32le = {ctype = "iq_format_u32_t", swap = ffi.abi("be"), offset = 2147483647.5,  scale = 1.0/2147483647.5},
-        u32be = {ctype = "iq_format_u32_t", swap = ffi.abi("le"), offset = 2147483647.5,  scale = 1.0/2147483647.5},
-        s32le = {ctype = "iq_format_s32_t", swap = ffi.abi("be"), offset = 0,             scale = 1.0/2147483647.5},
-        s32be = {ctype = "iq_format_s32_t", swap = ffi.abi("le"), offset = 0,             scale = 1.0/2147483647.5},
-        f32le = {ctype = "iq_format_f32_t", swap = ffi.abi("be"), offset = 0,             scale = 1.0},
-        f32be = {ctype = "iq_format_f32_t", swap = ffi.abi("le"), offset = 0,             scale = 1.0},
-        f64le = {ctype = "iq_format_f64_t", swap = ffi.abi("be"), offset = 0,             scale = 1.0},
-        f64be = {ctype = "iq_format_f64_t", swap = ffi.abi("le"), offset = 0,             scale = 1.0},
-    }
-
     if type(file) == "string" then
         self.filename = file
     elseif type(file) == "number" then
@@ -116,7 +57,7 @@ function IQFileSource:instantiate(file, format, rate, repeat_on_eof)
     end
 
     assert(format, "Missing argument #2 (format)")
-    self.format = assert(supported_formats[format], "Unsupported format (\"" .. format .. "\")")
+    self.format = assert(format_utils.formats[format], "Unsupported format (\"" .. format .. "\")")
     self.rate = assert(rate, "Missing argument #3 (rate)")
     self.repeat_on_eof = repeat_on_eof or false
 
@@ -146,15 +87,8 @@ function IQFileSource:initialize()
     self.files[self.file] = true
 
     -- Create sample vectors
-    self.raw_samples = vector.Vector(ffi.typeof(self.format.ctype), self.chunk_size)
+    self.raw_samples = vector.Vector(self.format.complex_ctype, self.chunk_size)
     self.out = types.ComplexFloat32.vector()
-end
-
-local function swap_bytes(x)
-    local len = ffi.sizeof(x.bytes)
-    for i = 0, (len/2)-1 do
-        x.bytes[i], x.bytes[len-i-1] = x.bytes[len-i-1], x.bytes[i]
-    end
 end
 
 function IQFileSource:process()
@@ -177,8 +111,8 @@ function IQFileSource:process()
     -- Perform byte swap for endianness if needed
     if self.format.swap then
         for i = 0, num_samples-1 do
-            swap_bytes(self.raw_samples.data[i].real)
-            swap_bytes(self.raw_samples.data[i].imag)
+            format_utils.swap_bytes(self.raw_samples.data[i].real)
+            format_utils.swap_bytes(self.raw_samples.data[i].imag)
         end
     end
 
@@ -186,8 +120,8 @@ function IQFileSource:process()
     local out = self.out:resize(num_samples)
 
     for i = 0, num_samples-1 do
-        out.data[i].real = (self.raw_samples.data[i].real.value - self.format.offset)*self.format.scale
-        out.data[i].imag = (self.raw_samples.data[i].imag.value - self.format.offset)*self.format.scale
+        out.data[i].real = (self.raw_samples.data[i].real.value - self.format.offset)/self.format.scale
+        out.data[i].imag = (self.raw_samples.data[i].imag.value - self.format.offset)/self.format.scale
     end
 
     return out

ec4c1ebca70336bcc9fdbe30fa47a2e7b71d5c1e
Vanya A. Sergeev <v@sergeev.io>
2019-12-02 22:22:03 -0600

    core/platform: add memchr() to ffi cdefs

--- radio/core/platform.lua
+++ radio/core/platform.lua
@@ -60,6 +60,7 @@ ffi.cdef[[
 ffi.cdef[[
     void *memmove(void *dest, const void *src, size_t n);
     int memcmp(const void *s1, const void *s2, size_t n);
+    void *memchr(const void *s, int c, size_t n);
 ]]
 
 -- POSIX File Descriptor I/O

b3b4821546d053891ca160851b4cfb1715b4e46a
Vanya A. Sergeev <v@sergeev.io>
2019-12-02 22:46:16 -0600

    blocks/signal/delay: use ffi.copy instead of memcpy

--- radio/blocks/signal/delay.lua
+++ radio/blocks/signal/delay.lua
@@ -40,10 +40,6 @@ function DelayBlock:initialize()
     self.out = data_type.vector()
 end
 
-ffi.cdef[[
-void *memcpy(void *dest, const void *src, size_t n);
-]]
-
 function DelayBlock:process(x)
     local out = self.out:resize(x.length)
 
@@ -51,21 +47,21 @@ function DelayBlock:process(x)
         -- Input is shorter than our state
 
         -- Shift out current state
-        ffi.C.memcpy(out.data, self.state.data, out.length*ffi.sizeof(self.state.data[0]))
+        ffi.copy(out.data, self.state.data, out.length*ffi.sizeof(self.state.data[0]))
         -- Shift down state samples
         ffi.C.memmove(self.state.data, self.state.data[out.length], (self.state.length - out.length)*ffi.sizeof(self.state.data[0]))
         -- Shift input into state
-        ffi.C.memcpy(self.state.data[self.state.length - out.length], x.data, x.length*ffi.sizeof(self.state.data[0]))
+        ffi.copy(self.state.data[self.state.length - out.length], x.data, x.length*ffi.sizeof(self.state.data[0]))
     else
         -- Input is longer than our state
 
         -- Shift out all of current state
-        ffi.C.memcpy(out.data, self.state.data, self.state.length*ffi.sizeof(self.state.data[0]))
+        ffi.copy(out.data, self.state.data, self.state.length*ffi.sizeof(self.state.data[0]))
         -- Shift out part of the input
-        ffi.C.memcpy(out.data[self.state.length], x.data, (out.length - self.state.length)*ffi.sizeof(self.state.data[0]))
+        ffi.copy(out.data[self.state.length], x.data, (out.length - self.state.length)*ffi.sizeof(self.state.data[0]))
         -- Shift remainder of the input into the state
         self.state:resize(x.length - (out.length - self.state.length))
-        ffi.C.memcpy(self.state.data, x.data[out.length - self.state.length], (x.length - (out.length - self.state.length))*ffi.sizeof(self.state.data[0]))
+        ffi.copy(self.state.data, x.data[out.length - self.state.length], (x.length - (out.length - self.state.length))*ffi.sizeof(self.state.data[0]))
     end
 
     return out

fdfcf3628d9a90f69638bf03f3f0ae348d91ac89
Vanya A. Sergeev <v@sergeev.io>
2019-12-11 20:08:35 -0600

    tests/composites/tuner: add conditional epsilon on liquid

--- tests/composites/tuner_spec.py
+++ tests/composites/tuner_spec.py
@@ -19,5 +19,6 @@ def generate():
     vectors.append(TestVector([0.2, 0.1, 5], [x], process(0.2, 0.1, 5, x), "0.2 offset, 0.1 bandwidth, 5 decimation, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
     vectors.append(TestVector([-0.2, 0.1, 5], [x], process(-0.2, 0.1, 5, x), "-0.2 offset, 0.1 bandwidth, 5 decimation, 256 ComplexFloat32 input, 256 ComplexFloat32 output"))
 
+    # FIXME liquid-dsp implementation has less precision (1e-3)
     # FIXME why does this need 1e-5 epsilon?
-    return CompositeSpec("TunerBlock", vectors, 1e-5)
+    return CompositeSpec("TunerBlock", vectors, "(radio.platform.features.liquid and not radio.platform.features.volk) and 1e-3 or 1e-5")

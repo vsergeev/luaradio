fa1c2e47aeb499229766e1f09218a6f74d625612
Vanya A. Sergeev <v@sergeev.io>
2020-11-01 01:36:27 -0600

    utilities/math_utils: add is_pow2() function

--- radio/utilities/math_utils.lua
+++ radio/utilities/math_utils.lua
@@ -4,6 +4,7 @@
 -- @module radio.utilities.math_utils
 
 local math = require('math')
+local bit = require('bit')
 
 ---
 -- Calculate the log2 ceiling.
@@ -15,4 +16,14 @@ local function ceil_log2(x)
     return math.ceil(math.log(x, 2))
 end
 
-return {ceil_log2 = ceil_log2}
+---
+-- Test if a number is a power of two.
+--
+-- @internal
+-- @function is_pow2
+-- @tparam number x Value
+local function is_pow2(x)
+    return x > 0 and bit.band(x, x - 1) == 0
+end
+
+return {ceil_log2 = ceil_log2, is_pow2 = is_pow2}
